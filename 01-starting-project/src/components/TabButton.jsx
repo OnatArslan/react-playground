86dfc12706708c441241f13edafa8da7fec843ef
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-09-13 01:30:15 +0300

    Commit for changes

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,9 +1,11 @@
 export default function TabButton(props) {
-  function handleClick() {}
+  function handleClick() {
+    console.log(`Hello world`);
+  }
   return (
     <li>
       {/* props.children refer to inner text of button element in App.jsx */}
-      <button onClick>{props.children}</button>
+      <button onClick={handleClick}>{props.children}</button>
     </li>
   );
 }
